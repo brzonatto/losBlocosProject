4f6c7ee4255b255bd3a2bf39aa085d541e25a57b
Brunno Zonatto <brzonatto@gmail.com>
2021-07-07 13:18:28 -0300

    ajustes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,13 +3,14 @@ const multer = require('multer')
 const multerConfig = require('./config/multer')
 const jimpConfig = require('./config/jimp')
 
-routes.post('/upload', multer(multerConfig).single('file'), (req, res) => {
-    console.log(req.file)    
-    return res.json({ process: 'Sucess' })
+routes.post('/upload', multer(multerConfig).single('file'), (req, res) => {    
+    console.log(req.file)          
+    return res.json({ process: 'Sucess' })   
 })
 
-routes.post('/teste', (req, res) => {
-    jimpConfig.resizeGameImage('tmp/uploads/gameImage.jpg')
+routes.post('/make', (req, res) => {  
+             
+    jimpConfig.resizeGameImage('tmp/uploads/gameImage.jpg')    
     return res.json({ process: 'Sucess' })
 })
 
--- frontend/src/components/pages/jsx/Publicity.jsx
+++ frontend/src/components/pages/jsx/Publicity.jsx
@@ -5,33 +5,49 @@ import axios from 'axios'
 
 
 export function Publicity() {
-    
-
+    const [golsPro, setGolsPro] = useState()
+    const [golsContra, setGolsContra] = useState()
     const [file, setFile] = useState()
+
     const teste = event => {
-       const data = new FormData()
-       data.append('file', file)
-       
+        const data = new FormData()
+        data.append('file', file)
+
         axios.post('http://localhost:3000/upload', data)
             .then(res => console.log(res))
             .catch(err => console.log(err))
-    }    
+    }
 
-    return (        
+    const teste2 = event => {
+        const data = new FormData()
+        data.append('golsPro', golsPro)
+        data.append('golsContra', golsContra)
+
+        axios.post('http://localhost:3000/make', data)
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+
+    return (
         <Main>
-            
             <div>
-            <div>Publicidade</div>
-            <hr />
-            <p>Instagram</p>
-            <form action="#">
+                <div>Publicidade</div>
+                <hr />
+                <p>Instagram</p>
                 <input type="file" name="file" onChange={event => {
                     const file = event.target.files[0]
                     setFile(file)
-                }}/>
-            </form>
-            <button onClick={teste}>Submit</button>
-            <button>make</button>
+                }} /> <br />
+                <button onClick={teste}>Submit</button><br /><br />
+                <input type="text" name="golsPro" onChange={event => {
+                    const { value } = event.target
+                    setGolsPro(value)
+                }} />
+                <input type="text" name="golsContra" onChange={event => {
+                    const { value } = event.target
+                    setGolsContra(value)
+                }} />  
+                <button onClick={teste2}>make</button>
             </div>
         </Main>
     )
