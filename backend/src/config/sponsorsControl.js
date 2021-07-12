8091e92efc00f1517308eff3a48d56e00dd899ff
Brunno Zonatto <brzonatto@gmail.com>
2021-07-12 17:54:25 -0300

    conexao front e back mongodb

--- backend/src/config/sponsorsControl.js
+++ backend/src/config/sponsorsControl.js
@@ -4,23 +4,34 @@ module.exports = {
     async index(req, res) {
         const sponsor = await Sponsors.find()
         res.json(sponsor)
-    },    
+    },
     async create(req, res) {
         const { name_sponsor } = req.body
         let data = {}
 
-        let sponsor = await Sponsors.findOne({name_sponsor})
+        let sponsor = await Sponsors.findOne({ name_sponsor })
         if (!sponsor) {
-            data = {name_sponsor}
-            sponsor = await Sponsors.create(data)            
-            return res.status(200).json(sponsor)            
-        } else {            
+            data = { name_sponsor }
+            sponsor = await Sponsors.create(data)
+            return res.status(200).json(sponsor)
+        } else {
             return res.status(500).json(sponsor)
         }
     },
-    async details(req, res) {        
+    async details(req, res) {
         const { _id } = req.params
         const sponsor = await Sponsors.findOne({ _id })
+        res.json(sponsor)
+    },
+    async delete(req, res) {
+        const { _id } = req.params
+        const sponsor = await Sponsors.findByIdAndDelete({ _id })
+        return res.json(sponsor)
+    },
+    async update(req, res) {
+        const { _id, name_sponsor } = req.body
+        const data = { name_sponsor }
+        const sponsor = await Sponsors.findOneAndUpdate({ _id }, data, { new: true })
         res.json(sponsor)
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,24 +5,24 @@ const jimpConfig = require('./config/jimp')
 const Sponsors = require('./config/sponsorsControl')
 
 routes.post('/upload', multer(multerConfig).single('file'), (req, res) => {    
-    console.log(req.body.golsPro)
-    console.log(req.file)          
-    return res.json({ process: 'Sucess' })   
+    return res.json(req.file)   
 })
 
-routes.post('/make', (req, res) => { 
-    console.log(req.body)     
+routes.post('/make', (req, res) => {
+    // console.log(req.body)          
     jimpConfig.resizeGameImage(
         req.body.golsPro, 
         req.body.golsContra, 
         req.body.gols, 
         req.body.assists, 
         'tmp/uploads/gameImage.jpg')    
-    return res.json({ process: 'Sucess' })
+    return res.json(req.body)
 })
 
 routes.post('/api/sponsor', Sponsors.create)
 routes.get('/api/sponsor', Sponsors.index)
-routes.get('/api/sponsor.details', Sponsors.details)
+routes.get('/api/sponsor.details/:_id', Sponsors.details)
+routes.delete('/api/sponsor/:_id', Sponsors.delete)
+routes.put('/api/sponsor', Sponsors.update)
 
 module.exports = routes
