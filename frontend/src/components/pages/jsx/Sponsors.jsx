f4061246be63483f7bd244b9fc24503e67d22df0
Brunno Zonatto <brzonatto@gmail.com>
2021-07-15 12:32:09 -0300

    ajustes

--- frontend/src/components/pages/jsx/Sponsors.jsx
+++ frontend/src/components/pages/jsx/Sponsors.jsx
@@ -2,41 +2,20 @@ import React from 'react'
 import Main from '../../templates/jsx/Main'
 import IndexSponsor from './IndexSponsor'
 import FormSponsors from './FormSponsors'
-import { Grid, makeStyles } from '@material-ui/core'
 import { Typography, Divider } from '@material-ui/core'
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1
-    },
-    paper: {
-        padding: theme.spacing(2)
-    }
-}))
-
-export function Sponsors() {
-    const classes = useStyles()
+export function Sponsors() {    
     return (
         <Main>
-            <div className={classes.root}>
-                <Grid spacing={3} container direction="column">
-                    <Grid xs={12} container direction="column" justifyContent="flex-start" alignItems="flex-start">
-                        <Typography variant="h3">
-                            Patrocinadores
-                        </Typography>
-                        <Typography variant="subtitle1">
-                            Gerenciador de Patrocinadores
-                        </Typography>
-                    </Grid>
-                    <Divider />
-                    <Grid xs={12} container direction="row-reverse">
-                        <FormSponsors />
-                    </Grid>
-                    <Grid container xs={12}>
-                        <IndexSponsor />
-                    </Grid>
-                </Grid>
-            </div>
+            <Typography variant="h3">
+                Patrocinadores
+            </Typography>
+            <Typography variant="subtitle1">
+                Gerenciador de Patrocinadores
+            </Typography>
+            <Divider />
+            <FormSponsors />
+            <IndexSponsor />
         </Main>
     )
 }
